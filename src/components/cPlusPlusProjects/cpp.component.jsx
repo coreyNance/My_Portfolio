ce8040253c72f9d2d7d2acf95522052b2ef06a04
Corey Nance <coreynance83@gmail.com>
2023-08-28 15:13:39 -0400

    adding cpp component

--- src/components/cPlusPlusProjects/cpp.component.jsx
+++ src/components/cPlusPlusProjects/cpp.component.jsx
@@ -3,6 +3,7 @@ import './cpp.styles.scss';
 import testPic from '../../Assets/images/test_image.png';
 import carQuiz from '../../Assets/images/codeQuizBig.png';
 import dPlanner from '../../Assets/images/DailyPlannerBig.png';
+import SoftReverse from '../cPlusPlusProjects/component_images/CS410_Software_Reverse.png';
 
 
 function CppCom() {
@@ -14,14 +15,15 @@ function CppCom() {
                 <h3>Project links will take you to github repository.</h3>
                 
               </center>
-              <a href ="https://github.com/coreyNance/Car_Enthusiast_Quiz" target = "_blank" className='ImageTextContainer'>
+              <a href ="https://github.com/coreyNance/CS-410-Software-Reverse-Engineering-" target = "_blank" className='ImageTextContainer'>
                 <div className='projectImage'>
-                <img className = "projectLeftImage" src={carQuiz} alt = "JavaScript Project."/>
+                <img className = "projectLeftImage" src={SoftReverse} alt = "JavaScript Project."/>
 
                 </div>
                 <div className='projectText'>
-                    <center><h2>Car Enthusiast Quiz Application</h2></center>
-                    <p>A program created as a car quiz with responsive design ques in mind. Time is deducted for wrong answers and high scores are saved and can be cleared.  HTML, CSS, JavaScript and Local storage were used for this assignment. This project showcases a mission-driven skill to create an application that utilizes JavaScript and CSS to dynamically show and hide features.
+                    <center><h2>Software-Reverse-Engineering</h2></center>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
                     </p>
                 </div>
 
@@ -31,12 +33,12 @@ function CppCom() {
 
             <a href ="https://github.com/coreyNance/Daily_Planner" target = "_blank" className='ImageTextContainer'>
                 <div className='projectText'>
-                <center><h2>Daily Planner Application</h2></center>
-                    <p>A calendar application that allows a user to save events for each hour of the day. This app runs in the browser and features dynamically updated HTML and CSS powered by jQuery. Moment.js library is used for the date and time.
+                <center><h2>Test Image</h2></center>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                     </p>
                 </div>
                 <div className='projectImage'>
-                <img className = "projectLeftImage" src={dPlanner} alt = "JavaScript Project."/>
+                <img className = "projectLeftImage" src={testPic} alt = "JavaScript Project."/>
 
                 </div>
 
